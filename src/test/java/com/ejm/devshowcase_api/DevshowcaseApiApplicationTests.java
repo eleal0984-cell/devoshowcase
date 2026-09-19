9ddd56fe5df793878aae19ecd1b652946418648a
Érika Leal <eleal0984@gmail.com>
2026-09-19 09:09:59 -0300

    Configuração inicial do projeto

---
+++ src/test/java/com/ejm/devshowcase_api/DevshowcaseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejm.devshowcase_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevshowcaseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
